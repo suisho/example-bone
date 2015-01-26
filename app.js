dc89d211cb7c5e8ec50507e2a3b7f406faadcad4
suisho <suisho000000@gmail.com>
2015-01-26 09:44:21 +0900

    add

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-var $ = require('jquery')(window)
+var $ = require('jquery')
 var checkboxTpl = require("./checkbox.hbs")
 var Backbone = require("backbone")
 Backbone.$ = $
@@ -6,35 +6,78 @@ Backbone.$ = $
 var Marionette = require("backbone.marionette")
 
 var Pref = Backbone.Model.extend({
+  defaults: {
+    "active" : false
+  }
 })
 
 var PrefGroup = Backbone.Collection.extend({
-  model: Pref
+  model: Pref,
+  activePrefs : function(){
+    var prefs = this.filter(function(item){
+      return item.get("active") === true
+    })
+    return new PrefGroup(prefs)
+  }
 })
 
 var PrefCheckView = Marionette.ItemView.extend({
+  ui : {
+    checkbox : "input.checkbox"
+  },
+  events : {
+    'change @ui.checkbox' : 'changeCheck'
+  },
+  changeCheck : function(e){
+    this.model.set("active", this.ui.checkbox.is(":checked") )
+    this.trigger("check:changed")
+  },
   template : function(model){
     return checkboxTpl({
-      name : model.name
+      name : model.name,
+      active : model.active || false
     })
   }
 })
 
+var prefGroup = new PrefGroup([
+  new Pref({name : "北海道", active: true}),
+  new Pref({name : "山形県"})
+])
+  
 var PrefGroupView = Marionette.CollectionView.extend({
-  childView : PrefCheckView
+  el : "#modal",
+  childView : PrefCheckView,
+  collection : prefGroup,
+  childEvents : {
+    'check:changed' : function(){
+    }
+  }
+})
+var PrefLabelView = Marionette.CollectionView.extend({
+  el : "#labels",
+  childView : PrefCheckView,
+  collection : prefGroup.activePrefs(),
+  childEvents : {
+    'check:changed' : function(){
+      this.collection = prefGroup.activePrefs()
+      console.log(this.collection)
+      this.render()
+    }
+  }
 })
 
 var PanelApp = Marionette.Application.extend({
   initialize : function(){
-    var prefGroup = new PrefGroup([
-      new Pref({name : "北海道"}),
-      new Pref({name : "山形県"})
-    ])
+    var prefGroupView = new PrefGroupView()
+    var prefLabelView = new PrefLabelView()
+    prefGroupView.render()
+    prefLabelView.render()
+    
   }
 })
 
 var panel = new PanelApp({
   container : "#container"
 })
-
 panel.start()
